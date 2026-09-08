74d4e66a2e343e8d57786160e5f703f0d7a74f21
Ahmed Amer <ahmedamerxx5@gmail.com>
2026-09-08 17:14:00 +0200

    question 1

--- assigment3/Program.cs
+++ assigment3/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using System;
+namespace assignment3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+            string title = "Refactoring";
+            switch (title)
+            {
+                case "Clean Code":
+                    Console.WriteLine("Great Choice!");
+                    break;
+                 
+                case "Refactoring":
+                    Console.WriteLine("Nice Pick!");
+                    break;
+
+                default:
+                    Console.WriteLine("Never Heared of it");
+                    break;
+
+            }
+            #endregion
+        }
+    }
+}
